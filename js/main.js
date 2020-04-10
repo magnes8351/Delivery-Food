01cf72c373f45c6ed4b89f58202a3a82c21f4724
oneren37 <oneren37@gmail.com>
2020-04-10 22:42:27 +0300

    пофикшен баг

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-const cartButton = document.querySelector(".cart-button");
-const modal = document.querySelector("modal");
-const close = document.querySelector("close");
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
 
-cartButton.addEventListener('click', function(event){
-    modal.classList.add("is open");
-})
+cartButton.addEventListener("click", function(event) {
+    modal.classList.add("is-open");
+    console.log("Привет ");
+});
 
-close.addEventListener('click', function(event){
-    modal.classList.remove("is open");
-})
-
-new WOW().init();
+close.addEventListener("click", function(event) {
+    modal.classList.remove("is-open");
+});
