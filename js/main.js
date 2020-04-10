77fc3a24663ea65b262c9df2cfa0020036e4b29c
magnes8351 <63446740+magnes8351@users.noreply.github.com>
2020-04-10 22:49:45 +0300

    1253

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const cartButton = document.querySelector(".cart-button");
-const modal = document.querySelector("modal");
-const close = document.querySelector("close");
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
 
-cartButton.addEventListener('click', function(event){
-    modal.classList.add("is open");
-})
+cartButton.addEventListener("click", function(event){
+    modal.classList.add("is-open");
+});
 
-close.addEventListener('click', function(event){
-    modal.classList.remove("is open");
-})
+close.addEventListener("click", function(event){
+    modal.classList.remove("is-open");
+});
 
 new WOW().init();
